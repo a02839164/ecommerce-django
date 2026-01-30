e0d6e8a55d53f4459fb689bf03775a5731f5976a
cake <a02839164@gmail.com>
2026-01-30 23:58:50 +0800

    fix: correct env file name in settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(DEBUG=(bool, False))
 
-environ.Env.read_env(BASE_DIR / '.fenv')
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
